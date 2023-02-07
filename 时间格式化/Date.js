f1a5f23429cf0e809bb2c4d9ce3f6b5e3a20cec6
2267258221@qq.com <2267258221@qq.com>
2023-02-07 15:43:13 +0800

    Date

--- 时间格式化/Date.js
+++ 时间格式化/Date.js
@@ -3,7 +3,9 @@ const date2 = new Date(1000);
 const date3 = new Date("2022-08-08");
 const date4 = new Date(2022, 08, 08, 08, 08, 08, 08);
 
+// Unix 时间戳：它是一个整数值，表示自1970年1月1日00:00:00 UTC以来的毫秒数。
 const date5 = new Date().getTime();
 const date6 = new Date().valueOf();
 const date7 = +new Date();
-console.log(date7);
+const date8 = Date.now();
+console.log(date8);
