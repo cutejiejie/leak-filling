5a28cd93bd76c81eceac3a9e7dc9720823849adf
2267258221@qq.com <2267258221@qq.com>
2023-02-07 15:14:40 +0800

    Date

---
+++ Date.js
@@ -0,0 +1,9 @@
+const date1 = new Date();
+const date2 = new Date(1000);
+const date3 = new Date("2022-08-08");
+const date4 = new Date(2022, 08, 08, 08, 08, 08, 08);
+
+const date5 = new Date().getTime();
+const date6 = new Date().valueOf();
+const date7 = +new Date();
+console.log(date7);
