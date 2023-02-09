5a47c31888c4714791ba899d3871528bd24a4c05
2267258221@qq.com <2267258221@qq.com>
2023-02-09 13:47:58 +0800

    URL解析

---
+++ url解析/text.js
@@ -0,0 +1,24 @@
+const url =
+  "https://www.baidu.com/m?f=8&ie=utf-8&rsv_bp=1&tn=monline_3_dg&wd=session";
+/* function queryURLparams(url) {
+  let obj = {};
+  if (url.indexOf("?") < 0) return obj;
+  let arr = url.split("?");
+  url = arr[1];
+  let array = url.split("&");
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i].split("=");
+    obj[item[0]] = item[1];
+  }
+  return obj;
+}
+console.log(queryURLparams(url)); */
+
+function queryURLparams(url) {
+  if (url.indexOf("?") < 0) return obj;
+  let arr = url.split("?");
+  paramsString = arr[1];
+  const searchParams = new URLSearchParams(paramsString);
+  return Object.fromEntries(searchParams);
+}
+console.log(queryURLparams(url));
