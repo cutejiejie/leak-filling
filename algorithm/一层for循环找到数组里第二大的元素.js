aca90b4985de5e014f2371747ce6184e1815e019
2267258221@qq.com <2267258221@qq.com>
2023-02-12 10:14:57 +0800

    一层for循环找到数组里第二大的元素

--- algorithm/一层for循环找到数组里第二大的元素.js
+++ algorithm/一层for循环找到数组里第二大的元素.js
@@ -5,9 +5,9 @@ const arr = [4, 6, 5, 3, 9, 8, 4, 7, 11, 2];
 没使用额外空间 */
 const oneSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
-    if (arr[1] > arr[0]) {
-      arr.unshift(arr[1]);
-      arr.splice(1 + 1, 1);
+    if (i === 1 && arr[1] > arr[0]) {
+      arr.unshift(arr[i]);
+      arr.splice(i + 1, 1);
     } else if (arr[i] < arr[0] && arr[i] > arr[1]) {
       arr[1] = arr[i];
       arr.splice(i, 1);
