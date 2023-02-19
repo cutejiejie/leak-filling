413acb95f887f90e564d4434148d9e8b20df1c0a
2267258221@qq.com <2267258221@qq.com>
2023-02-19 11:33:10 +0800

    01_把一个数组旋转k步

---
+++ algorithm/01_把一个数组旋转k步.js
@@ -0,0 +1,27 @@
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+const rotate1 = (arr, step) => {
+  step = Math.abs(step % arr.length);
+  for (let i = 0; i < step; i++) {
+    const n = arr.pop();
+    if (n != null) {
+      arr.unshift(n);
+    }
+  }
+  return arr;
+};
+
+const res1 = rotate1(arr, 9);
+console.log(res1);
+
+const rotate2 = (arr, step) => {
+  step = Math.abs(step % arr.length);
+  const part1 = arr.slice(-step);
+  const part2 = arr.slice(0, arr.length - step);
+  arr = part2.concat(part1);
+  return arr;
+};
+
+const res2 = rotate1(arr2, 9);
+console.log(res2);
