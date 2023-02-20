d2b7895c6be2d37439444b6d0b192075f2b90d04
2267258221@qq.com <2267258221@qq.com>
2023-02-20 12:57:32 +0800

    07_字符串大小写转换【ASCII编码】

--- algorithm/07_字符串大小写转换.js
+++ algorithm/07_字符串大小写转换.js
@@ -1,15 +1,17 @@
 const string = "AbcDG123Hjl";
 const bigSmall = (str) => {
+  let res = "";
+  if (str.length === 0) return res;
   let reg1 = /[A-Z]/;
   let reg2 = /[a-z]/;
-  let res = "";
   for (let i = 0; i < str.length; i++) {
-    if (reg1.test(str[i])) {
-      res = res + str[i].toLowerCase();
-    } else if (reg2.test(str[i])) {
-      res = res + str[i].toUpperCase();
+    const c = str[i];
+    if (reg1.test(c)) {
+      res += c.toLowerCase();
+    } else if (reg2.test(c)) {
+      res += c.toUpperCase();
     } else {
-      res = res + str[i];
+      res += c;
     }
   }
   return res;
@@ -17,3 +19,25 @@ const bigSmall = (str) => {
 
 const result = bigSmall(string);
 console.log(result);
+
+/* ASCLL码 */
+const string2 = "AbcDG123Hjl";
+const bigSmall2 = (str) => {
+  let res = "";
+  for (let i = 0; i < str.length; i++) {
+    // const code = str.charCodeAt(i);
+    const c = str[i];
+    const code = c.charCodeAt(0);
+    if (65 <= code <= 90) {
+      res += c.toUpperCase();
+    } else if (97 <= code <= 122) {
+      res += c.toLowerCase();
+    } else {
+      res += c;
+    }
+  }
+  return res;
+};
+
+const result2 = bigSmall(string2);
+console.log(result2);
