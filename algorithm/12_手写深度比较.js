a034368f02e21cf396db521277094323f1401b0a
2267258221@qq.com <2267258221@qq.com>
2023-02-24 16:16:48 +0800

    待续

---
+++ algorithm/12_手写深度比较.js
@@ -0,0 +1,5 @@
+const obj1 = { a: 10, b: { x: 100, y: 200 }, d: { x: 190, y: 200 } };
+const obj2 = { a: 10, b: { x: 100, y: 200 }, d: { x: 100, y: 200 } };
+
+const arr1 = [1, 2, 3, [6, 5]];
+console.log(arr1.slice());
